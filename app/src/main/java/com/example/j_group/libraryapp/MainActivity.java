324950aeba0683e03c77234aeb0207cec183171b
syokenn334 <yannyaya@icloud.com>
2018-12-06 02:24:05 +0900

    ボタンの配置

--- app/src/main/java/com/example/j_group/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/j_group/libraryapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.j_group.libraryapp;
 
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button map_button = findViewById(R.id.map_button);
+        map_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogFragment;
+                dialogFragment = new Dialog_bookmap();
+                FragmentManager manager = getSupportFragmentManager();
+                dialogFragment.show(manager, "dialog");
+            }
+        });
     }
-
-
 }
