7b2386dfd32982c2d9d2f60103c833f4fb91b635
arnuro <hunterayumu0623@gmail.com>
2018-12-06 09:01:02 +0900

    a

---
+++ app/src/main/gen/com/example/j_group/libraryapp/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.j_group.libraryapp;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
